ee4505de76e8321242e24a926b2bb093f5992116
Nqobile Moyo <NqobileM@ssda.co.za>
2025-03-11 17:00:21 +0200

    First commint

---
+++ Ecommerce/Core/Entities/Identity/Address.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities.Identity
+{
+    [Keyless]
+    public class Address
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
